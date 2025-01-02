014082584d77295aed26146fcea8d4d60bfa0237
Sushi-Brando <joaovitorcortellini@gmail.com>
2025-01-02 01:50:17 -0300

    Update sidebar.php

--- elementos/sidebar/sidebar.php
+++ elementos/sidebar/sidebar.php
@@ -2,6 +2,7 @@
 $ads = [
   ['ad1.png', 'https://www.google.com.br/search?q=cabelo'],
   ['ad2.png', 'https://store.steampowered.com/app/2161700/Persona_3_Reload/'],
+  ['ad3.png'. 'https://especulamente.com.br'],
 ];
 $ad = $ads[array_rand($ads)];
 ?>
