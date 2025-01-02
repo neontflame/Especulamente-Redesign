543ae783fed7aed267e8c6a4860bec868bcf365b
Fupicat <fupicat@gmail.com>
2025-01-02 17:26:56 -0300

    squl

--- shhhh/especulamente.sql
+++ shhhh/especulamente.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 03/11/2024 às 01:44
+-- Tempo de geração: 02/01/2025 às 20:26
 -- Versão do servidor: 8.3.0
 -- Versão do PHP: 8.2.18
 
@@ -32,15 +32,47 @@ CREATE TABLE IF NOT EXISTS `convites` (
   `id` int NOT NULL AUTO_INCREMENT,
   `codigo` varchar(255) NOT NULL,
   `criado_por` int NOT NULL,
-  `usado_por` int NULL,
+  `usado_por` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_criado_por` (`criado_por`),
   KEY `FK_usado_por` (`usado_por`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
 
 --
--- Despejando dados para a tabela `convites`
+-- Estrutura para tabela `projetos`
 --
+
+DROP TABLE IF EXISTS `projetos`;
+CREATE TABLE IF NOT EXISTS `projetos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_criador` int NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `tipo` enum('dl','md','jg','bg','rt') NOT NULL,
+  `arquivos` text NOT NULL,
+  `arquivos_de_vdd` text NOT NULL,
+  `mitadas` int NOT NULL,
+  `sojadas` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `reacoes`
+--
+
+DROP TABLE IF EXISTS `reacoes`;
+CREATE TABLE IF NOT EXISTS `reacoes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo_de_reacao` enum('mitada','sojada') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_reator` int NOT NULL,
+  `tipo_de_reagido` enum('perfil','projeto') NOT NULL,
+  `id_reagido` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -54,12 +86,14 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `username` varchar(255) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `bio` varchar(1024) NOT NULL,
+  `mitadas` int NOT NULL,
+  `sojadas` int NOT NULL,
+  `pfp` varchar(255) NOT NULL,
+  `banner` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Despejando dados para a tabela `usuarios`
---
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
