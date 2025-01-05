1f3aca65d9df0cf8fd6e04b351e4106056500677
Neon T. Flame <neontflame@gmail.com>
2025-01-05 01:04:53 -0300

    eu sinto o css cursando por minhas veias
    agh.. e poderoso demais...
    - mais css na pagina de projetos (e mais info tb!)
    - paginaçao bonitinha

--- projetos/index.php
+++ projetos/index.php
@@ -11,31 +11,46 @@ $pages = projetos_tudo($projetos, $page);
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/elementos/header/header.php'; ?>
 
 <div class="container">
-  <?php /* include $_SERVER['DOCUMENT_ROOT'] . '/elementos/sidebar/sidebar.php'; */ ?>
+  <?php include $_SERVER['DOCUMENT_ROOT'] . '/elementos/sidebar/sidebar.php'; ?>
 
-  <div class="page_content" style="min-height: 486px; margin-left: 0">
+  <img src="/elementos/pagetitles/projetos.png" class="inside_page_content" style="padding: 0px; margin-left: 4px; margin-bottom: 7px;">
+	
+  <div class="page_content" style="min-height: 486px;">
     <div class="inside_page_content">
-      <h1>Projetos</h1>
-
+	
       <div class="projetos">
         <?php foreach ($projetos as $projeto) : ?>
           <div class="projeto">
+            <a href="<?= $config['URL'] ?>/projetos/ver.php?id=<?= $projeto->id ?>"><img src="/elementos/botaoTransferir.png"></a>
             <h2><?= $projeto->nome ?></h2>
             <p><?= $projeto->descricao ?></p>
-            <a href="<?= $config['URL'] ?>/projetos/ver.php?id=<?= $projeto->id ?>">Ver</a>
+            <p class="autorDeProjeto">carregado por <?= usuario_requestIDator($projeto->id_criador)->username ?></p>
           </div>
         <?php endforeach ?>
 
+		<!-- here be pagination -->
         <div class="pagination">
           <?php if ($page > 1) : ?>
-            <a href="<?= $config['URL'] ?>/projetos/?page=<?= $page - 1 ?>">Anterior</a>
+            <a href="<?= $config['URL'] ?>/projetos/?page=1">Início</a>
+			<p class="textinhoClaro">~</p>
+            <a href="<?= $config['URL'] ?>/projetos/?page=<?= $page - 1 ?>">« Anterior</a>
+			<p class="textinhoClaro">~</p>
           <?php endif ?>
-
-          <?php if ($page < $pages) : ?>
-            <a href="<?= $config['URL'] ?>/projetos/?page=<?= $page + 1 ?>">Próximo</a>
-          <?php endif ?>
+		  <?php if ($page == 1) : ?>
+		    <p class="textinhoClaro" style="margin-right: 4px;">Início ~ « Anterior ~ </a>
+		  <?php endif ?>
 
           <p>Página <?= $page ?> de <?= $pages ?></p>
+		  
+          <?php if ($page < $pages) : ?>
+			<p class="textinhoClaro">~</p>
+            <a href="<?= $config['URL'] ?>/projetos/?page=<?= $page + 1 ?>">Próximo »</a>
+			<p class="textinhoClaro">~</p>
+            <a href="<?= $config['URL'] ?>/projetos/?page=<?= $pages ?>">Fim</a>
+          <?php endif ?>
+		  <?php if ($page == $pages) : ?>
+		    <p class="textinhoClaro"> ~ Próximo » ~ Fim</a>
+		  <?php endif ?>
         </div>
       </div>
     </div>
