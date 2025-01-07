506b0d3a005fbb13c852c1de7ae671ba37c65bc8
Neon T. Flame <neontflame@gmail.com>
2025-01-07 01:59:50 -0300

    eu fiquei ate 2 da manha tentando implementar esse negocio
    - VEDOR D IMAGENS FUNCIONAL FEITO EM FLASH !! AAAAAAAAAAAAAAAAAAAAAAAAAA
    - todo: fazer um fallback pros browsers que nao suportam flash ?

--- projetos/vedorDImagem.php
+++ projetos/vedorDImagem.php
@@ -3,6 +3,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/shhhh/autoload.php';
 ?>
 
 <?php
+// isso aqui e uma api Ok !
 $tipos = array('.png', '.bmp', '.jpg', 'jpeg', '.gif', '.PNG', '.BMP', '.JPG', 'JPEG', '.GIF');
 
 if (!isset($_GET['id'])) {
@@ -13,13 +14,15 @@ $id = $_GET['id'];
 $projeto = projeto_requestIDator($id);
 // nao explodir hostinger
 
+header("Content-Type: text/plain; charset=utf-8");
+
 $arquivos = explode('\n', $projeto->arquivos);
 $arquivos_de_vdd = explode('\n', $projeto->arquivos_de_vdd);
 
 if ($_GET['modo'] == "internal") {
 	foreach ($arquivos as $i => $arquivo) {
 		if (in_array(substr($arquivo, -4), $tipos)) {
-			echo $arquivo;
+			echo $arquivo . "\n";
 		}
 	}
 }
@@ -27,7 +30,7 @@ if ($_GET['modo'] == "internal") {
 if ($_GET['modo'] == "outer") {
 	foreach ($arquivos_de_vdd as $i => $arquivo) {
 		if (in_array(substr($arquivo, -4), $tipos)) {
-			echo $arquivo;
+			echo $arquivo . "\n";
 		}
 	}
 }
--- projetos/ver.php
+++ projetos/ver.php
@@ -21,13 +21,21 @@ $arquivos_de_vdd = explode('\n', $projeto->arquivos_de_vdd);
   <div class="page_content" style="min-height: 486px; margin-left: 0">
     <div class="inside_page_content">
       <h1><?= $projeto->nome ?></h1>
-
+		
       <?php if ($projeto->tipo == 'dl') : ?>
+	  <!-- downloadavel -->
         <ul>
           <?php foreach ($arquivos as $i => $arquivo) : ?>
             <li><a href="<?= $config['URL'] ?>/static/projetos/<?= $projeto->id ?>/<?= $arquivo ?>" download="<?= $arquivos_de_vdd[$i] ?>"><?= $arquivos_de_vdd[$i] ?></a></li>
           <?php endforeach ?>
         </ul>
+		
+		<a href="<?= $config['URL'] ?>/projetos/zipar.php?id=<?= $projeto->id ?>">Baixar todos os arquivos!</a>
+      <?php endif ?>
+	  
+      <?php if ($projeto->tipo == 'md') : ?>
+	  <!-- midia -->
+        <object width="600" height="360" data="/elementos/vedorDImagem.swf"><param name="flashvars" value="server=<?= $config['URL'] ?>/&projectid=<?= $projeto->id ?>"/></object>
 		
 		<a href="<?= $config['URL'] ?>/projetos/zipar.php?id=<?= $projeto->id ?>">Baixar todos os arquivos!</a>
       <?php endif ?>
