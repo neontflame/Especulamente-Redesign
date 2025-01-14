66802fad46b1557cb55341435488326cfd66961f
Neon T. Flame <neontflame@gmail.com>
2025-01-14 18:27:50 -0300

    nvm

--- elementos/vedor_d_comentario/postarComentario.php
+++ elementos/vedor_d_comentario/postarComentario.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST)) {
   if (isset($_POST['comentario'])) {
 		$tipo = $_POST['tipo'];
 		$id = $_POST['id'];
-		$comentario = htmlspecialchars($_POST['comentario']);
+		$comentario = $_POST['comentario'];
 		$fio = $_POST['fio'] ?? 0;
 	  
 	    $rows = $db->prepare("INSERT INTO comentarios (id, id_comentador, id_coisa, tipo_de_coisa, texto, fio, data) VALUES (NULL, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP())");
