0b0984eaae1a1af96aef644b466fe40145b78537
Neon T. Flame <neontflame@gmail.com>
2025-02-27 19:30:35 -0300

    esperando em deus que isso funcione

--- shhhh/autoload.php
+++ shhhh/autoload.php
@@ -8,17 +8,19 @@ include $_SERVER['DOCUMENT_ROOT'] . '/elementos/reajor_d_reagida/rdr.php';
 function redirect($location)
 {
   header("Location: " . $location);
-  exit();
+  die();
 }
 
 function erro_404()
 {
   include '../404.php';
-  exit();
+  if(!isset($_SESSION)) { session_start(); }
+  die();
 }
 
 function erro_403()
 {
   include '../403.php';
-  exit();
+  if(!isset($_SESSION)) { session_start(); }
+  die();
 }
