ff267a49cb89d392b5c3ec6ab78d6e9510f5c4c0
Neon T. Flame <neontflame@gmail.com>
2025-04-15 15:19:01 -0300

    imagens engraçadonhas
    iuhu

--- projetos/ver.php
+++ projetos/ver.php
@@ -102,9 +102,71 @@ if (isset($usuario)) {
 
 			<?php if ($projeto->tipo == 'md') : ?>
 				<!-- midia -->
+				<div class="vedorDImagem">
+					<img src="/elementos/chillmaia.png" id="imagemAtual">
+					
+					<br>
+					
+					<div class="outrasImagens">
+					</div>
+					
+					<script>
+					var projid = 0;
+					var imagens = ["img1.png", "img2.png", "img3.png"];
+
+					function fazONegocioDasImagens() {
+						for (var imgcoiso = 0; imgcoiso < imagens.length; imgcoiso++) {
+							var img = document.createElement("img");
+							img.src = "/static/projetos/" + projid + "/" + imagens[imgcoiso];
+							img.className = "imagemCoiso";
+							img.id = imgcoiso;
+							img.onclick = function(){		
+								var quiamsas = document.getElementsByClassName('outrasImagens')[0].children;
+								
+								for (var kid = 0; kid < quiamsas.length; kid++) {
+									quiamsas[kid].className = "imagemCoiso";
+								}
+								clicCoiso(this.id)
+								this.className = "imagemCoiso desopaco";
+							};
+							document.getElementsByClassName("outrasImagens")[0].appendChild(img);
+						}
+						
+						document.getElementById("imagemAtual").src =  "/static/projetos/" + projid + "/" + imagens[0];
+					}
+
+					function clicCoiso(id) {
+						console.log('clic');
+						document.getElementById("imagemAtual").src = "/static/projetos/" + projid + "/" + imagens[id];
+					}
+
+
+					function carregarImagens(id) {
+					  var xhttp = new XMLHttpRequest();
+					  xhttp.onload = function () {
+						projid = id;
+						imagens = this.responseText.split("\n");
+						imagens.shift();
+						imagens.pop();
+						fazONegocioDasImagens();
+					  };
+					  xhttp.open(
+						"GET",
+						"/projetos/vedorDImagem.php?id=" +
+						  id + "&modo=internal",
+						true
+					  );
+					  xhttp.send();
+					}
+					
+					carregarImagens(<?= $projeto->id ?>);
+					</script>
+				</div>
+				<!--
 				<object width="600" height="360" data="/elementos/vedorDImagem.swf" allowfullscreen="true">
 					<param name="flashvars" value="server=<?= $config['URL'] ?>/&projectid=<?= $projeto->id ?>" />
 				</object>
+				-->
 			<?php endif ?>
 
 		</div>
