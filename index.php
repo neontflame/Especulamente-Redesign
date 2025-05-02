0e26226d1bc357229289811d7f055965503c88f4
Neon T. Flame <neontflame@gmail.com>
2025-05-01 21:24:39 -0300

    paginacio iniciacio
    acio acio

--- index.php
+++ index.php
@@ -4,12 +4,71 @@
 <div class="container">
   <?php include $_SERVER['DOCUMENT_ROOT'] . '/elementos/sidebar/sidebar.php'; ?>
 
-  <div class="page_content" style="height: 486px">
+  <div class="page_content" style="min-height: 486px">
     <div class="inside_page_content">
-      <p>Sejam todos bem-vindos ao</p>
-      <h1>Portal Especulamente!!</h1>
-      <p>Aqui você encontra os melhores Jogos, a melhor Arte, e os melhores TUDO que a Internet tem para oferecer!</p>
-      <p>Ainda estamos desenvolvendo o site, mas por enquanto, você já pode dar uma olhada nos nossos <a href="/projetos">Projetos</a>.</p>
+	  <style>
+	  .labelManeira {
+		  font-size:15px; 
+		  font-weight:bold;
+		  margin-top: 4px;
+		  margin-bottom: 4px;
+	  }
+	  </style>
+	  <p class="labelManeira">>> DESTAQUE</p>
+	  <div class="separador" style="margin-bottom: 8px;"></div>
+	  <a href="/projetos/ver.php?id=46"><img src="/elementos/destaques/vsespe.png"></a>
+	  <div class="separador"></div>
+	  <p class="labelManeira">>> PROJETOS RECENTES</p>
+	  <div class="separador" style="margin-bottom: 8px;"></div>
+	  <?php
+	  $projetos = [];
+
+	  $pages = coisos_tudo($projetos, 'projetos', 1, '', '', 3);
+	  ?>
+            <div class="projetos">
+              <?php foreach ($projetos as $projeto) : ?>
+				<?php if ($projeto->tipo == 'dl' || $projeto->tipo == 'bg') : ?>
+                <div class="projeto">
+				  <?php if ($projeto->tipo == 'dl') : ?>
+                  <a href="<?= $config['URL'] ?>/projetos/zipar.php?id=<?= $projeto->id ?>"><img src="/elementos/botaoTransferirProjetos.png"></a>
+                  <?php endif ?>
+				  <?php if ($projeto->tipo == 'bg') : ?>
+                  <a href="<?= $config['URL'] ?>/projetos/ver.php?id=<?= $projeto->id ?>"><img src="/elementos/botaoLerBlog.png"></a>
+                  <?php endif ?>
+				  <a class="autorDeProjeto" href="<?= $config['URL'] ?>/usuarios/<?= usuario_requestIDator($projeto->id_criador)->username ?>">
+                    feito por <?= usuario_requestIDator($projeto->id_criador)->username ?>
+                  </a>
+                  <h2><a href="<?= $config['URL'] ?>/projetos/ver.php?id=<?= $projeto->id ?>"><?= $projeto->nome ?></a></h2>
+                  <p><?= explode("\n", $projeto->descricao)[0] ?></p>
+                </div>
+				<?php endif ?>
+				<?php if ($projeto->tipo == 'jg' || $projeto->tipo == 'rt' || $projeto->tipo == 'md') : ?>
+                <div class="projeto" style="min-height:84px">
+				  <?php if ($projeto->tipo == 'rt') : ?>
+				  <a href="<?= $config['URL'] ?>/~<?= $projeto->arquivos_de_vdd ?>"><img src="/elementos/botaoVerResto.png"></a>
+				  <?php endif ?>
+				  
+				  <?php if ($projeto->tipo == 'jg') : ?>
+				  <a href="<?= $config['URL'] ?>/projetos/ver.php?id=<?= $projeto->id ?>"><img src="/elementos/botaoJogar.png"></a>
+				  <?php endif ?>
+				  
+				  <?php if ($projeto->tipo == 'md') : ?>
+				  <a href="<?= $config['URL'] ?>/projetos/ver.php?id=<?= $projeto->id ?>"><img src="/elementos/botaoVerMidia.png"></a>
+				  <?php endif ?>
+				  
+				  <!-- nem tudo precisa ter uma thumbnail! -->
+				  <?php if ($projeto->thumbnail != null) { ?>
+                  <a href="<?= $config['URL'] ?>/projetos/ver.php?id=<?= $projeto->id ?>" style="float:left; margin-right: 8px"><img style="max-width:96px; height:72px" src="/static/projetos/<?= ($projeto->id) ?>/thumb/<?= ($projeto->thumbnail) ?>"></a>
+				  <?php } ?>
+                  <a class="autorDeProjeto" href="<?= $config['URL'] ?>/usuarios/<?= usuario_requestIDator($projeto->id_criador)->username ?>">
+                    por <?= usuario_requestIDator($projeto->id_criador)->username ?>
+                  </a>
+                  <h2><a href="<?= $config['URL'] ?>/projetos/ver.php?id=<?= $projeto->id ?>"><?= $projeto->nome ?></a></h2>
+                  <p><?= explode("\n", $projeto->descricao)[0] ?></p>
+                </div>
+				<?php endif ?>
+              <?php endforeach ?>
+            </div>
     </div>
   </div>
 </div>
