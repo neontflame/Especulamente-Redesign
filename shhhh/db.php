200e273c6021aba59d041d11388deb390ddd402e
Neon T. Flame <neontflame@gmail.com>
2025-05-16 16:00:13 -0300

    acho q teve um oversight aqui

--- shhhh/db.php
+++ shhhh/db.php
@@ -682,16 +682,10 @@ function criar_projeto($id_criador, $nome, $descricao, $tipo, $arquivos, $arquiv
 
 	// EXPLODIR HOSTINGER
 	// se for rt ele coloca o nome da pasta em $arquivos_de_vdd
-	$arquivos_de_vdd = ($arquivos != null && !is_string($arquivos)) ? implode('\n', $arquivos['name']) : (is_string($arquivos) ? $arquivos : '');
+	$arquivos_de_vdd = ($arquivos != null && !is_string($arquivos)) ? implode('\n', $arquivos['name']) : (is_string($arquivos) ? $arquivos : null);
 
 	if ($arquivos == null && $arquivoVivel == null && ($tipo != 'rt' && $tipo != 'bg')) {
-		return "Comeram seus arquivos?";
-	}
-	
-	if ($tipo == 'jg') {
-		if ($arquivos == null && $arquivos_de_vdd == '' && $arquivoVivel == null) {
-			return "§Você precisa anexar ALGUM tipo de arquivo!";
-		}
+		return "§Comeram seus arquivos?";
 	}
 	
 	$rows = $db->prepare("INSERT INTO projetos (id_criador, nome, descricao, tipo, arquivos_de_vdd) VALUES (?, ?, ?, ?, ?)");
