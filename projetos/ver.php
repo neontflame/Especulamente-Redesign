50b504110359d8cd4774724632b14c395ffc1cfc
Neon T. Flame <76576101+neontflame@users.noreply.github.com>
2025-05-16 20:59:50 -0300

    uhhhhhhhhhhhh

--- projetos/ver.php
+++ projetos/ver.php
@@ -68,9 +68,12 @@ include $_SERVER['DOCUMENT_ROOT'] . '/elementos/header/header.php'; ?>
 						
 						<script>
 						var flashio = document.getElementsByClassName('jogo')[0].children[0];
-						flashio.width = 620;
-						flashio.height = parseInt(flashio.TGetProperty('/', 9) * (620 / flashio.TGetProperty('/', 8)))
-						console.log('browser véio fix !');
+						
+						if (flashio.PercentLoaded() > 99) {
+							flashio.width = 620;
+							flashio.height = parseInt(flashio.TGetProperty('/', 9) * (620 / flashio.TGetProperty('/', 8)))
+							console.log('browser véio fix !');
+						}
 						</script>
 					<?php endif; ?>
 					<?php if (str_ends_with($arquivo_vivel[0], '.zip')) : ?>
