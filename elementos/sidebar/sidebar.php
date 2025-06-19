64a26624dd77b84d3b24267e251fe52aa0b11c51
Neon T. Flame <neontflame@gmail.com>
2025-06-19 16:40:27 -0300

    rankios !!!!! yeah
    Woo Hoo

--- elementos/sidebar/sidebar.php
+++ elementos/sidebar/sidebar.php
@@ -80,7 +80,7 @@ $ad = $ads[array_rand($ads)];
 	<div class="listinhaDeForinhos">
 		<?php
 		$posts = [];
-		$rows = $db->prepare("SELECT * FROM forum_posts WHERE id_resposta = -1 ORDER BY dataBump DESC LIMIT 4");
+		$rows = $db->prepare("SELECT * FROM forum_posts WHERE id_resposta = -1 ORDER BY dataBump DESC LIMIT 5");
 		$rows->execute();
 		
 		while ($row = $rows->fetch(PDO::FETCH_OBJ)) {
---
+++ ranking/index.php
@@ -0,0 +1,80 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/shhhh/autoload.php'; 
+
+$sortCoiso = [
+'mitada' => 'mitadas DESC',
+'sojada' => 'sojadas DESC',
+'davecoin' => 'davecoins DESC'
+];
+
+$sortCoisitos = (isset($_GET['sort']) && array_key_exists($_GET['sort'], $sortCoiso)) ? $_GET['sort'] : 'mitada';
+
+?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/elementos/header/header.php'; ?>
+
+<div class="container">
+	<style>
+	.sortsCoiso {
+	  color: #87A0DB;
+	  text-align: center;
+	  margin-top: 2px;
+	}
+
+	.sortsCoiso a {
+	  color: #87A0DB;
+	  text-decoration: none;
+	}
+
+	.sortsCoiso a:hover {
+	  text-decoration: underline;
+	}
+
+	.sortsCoiso b {
+	  color: black;
+	}
+	</style>
+	<?php 
+	$forum_no_lado = true;
+	$esconder_ad = true;
+	include $_SERVER['DOCUMENT_ROOT'] . '/elementos/sidebar/sidebar.php'; ?>
+
+	<div class="page_content" style="min-height: 556px">
+		<div class="inside_page_content">
+			<img src="/elementos/pagetitles/rankings.png" style="margin-top: -5px; margin-left: -5px; margin-right: -5px;">
+			<div class="sortsCoiso">
+			<?php if ($sortCoisitos != 'mitada') { ?><a href="/ranking?sort=mitada">mais mitados</a><?php } else { ?><b>mais mitados</b><?php } ?>
+			- 
+			<?php if ($sortCoisitos != 'sojada') { ?><a href="/ranking?sort=sojada">mais sojados</a><?php } else { ?><b>mais sojados</b><?php } ?>
+			- 
+			<?php if ($sortCoisitos != 'davecoin') { ?><a href="/ranking?sort=davecoin">com mais davecoins</a><?php } else { ?><b>com mais davecoins</b><?php } ?>
+			</div>
+			
+			<div class="separador" style="margin-bottom: 8px; border-color:#D2EDFF"></div>
+			<?php
+			$usuarios = [];
+
+			$pages = coisos_tudo($usuarios, 'usuarios', 1, '', '', 1000, $sortCoiso[$sortCoisitos]);
+			?>
+			<div class="usuariosRanking">
+				<?php
+				$lugar = 1;
+				foreach ($usuarios as $usuario) { ?>
+				<div class="rankeado">
+					<span class="lugar<?php if ($lugar < 4) { echo $lugar; } ?>"><?= $lugar ?>º</span>
+					<img src="<?php
+                if ($usuario->pfp != null) {
+                  echo '/static/pfps/' . $usuario->pfp;
+                } else {
+                  echo '/static/pfp_padrao.png';
+                } ?>">
+					<span class="username"><?= $usuario->username ?></span>
+					<span class="infoExtra">com <?= $usuario->mitadas ?> <?= $sortCoisitos ?>s</span>
+				</div>
+				<?php 
+				$lugar += 1;
+				} ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/elementos/footer/footer.php'; ?>
